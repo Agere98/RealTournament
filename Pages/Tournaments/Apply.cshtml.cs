59f12ce27957de6386d10dfeedf7a2fedb5de161
Agere98 <44338036+Agere98@users.noreply.github.com>
2020-06-20 14:53:17 +0200

    Add Apply page

---
+++ Pages/Tournaments/Apply.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RealTournament.Areas.Identity.Data;
+using RealTournament.Data;
+using RealTournament.Models;
+
+namespace RealTournament.Pages.Tournaments
+{
+    [Authorize]
+    public class ApplyModel : PageModel
+    {
+        private readonly RealTournamentContext _context;
+        private readonly UserManager<RealTournamentUser> _userManager;
+
+        public ApplyModel(RealTournamentContext context, UserManager<RealTournamentUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Tournament = await _context.Tournament.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Tournament == null)
+            {
+                return NotFound();
+            }
+
+            TournamentId = id.Value;
+            return Page();
+        }
+
+        public Tournament Tournament { get; set; }
+        [BindProperty]
+        public int TournamentId { get; set; }
+        [BindProperty]
+        public string LicenseNumber { get; set; }
+        [BindProperty]
+        public int CurrentRanking { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var participant = new Participant()
+            {
+                TournamentId = TournamentId,
+                UserId = _userManager.GetUserId(User),
+                LicenseNumber = LicenseNumber,
+                Ranking = CurrentRanking
+            };
+
+            try
+            {
+                await _context.Participant.AddAsync(participant);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                ViewData["errMsg"] = "Invalid participant data";
+                return await OnGetAsync(TournamentId);
+            }
+
+            return RedirectToPage("./Details", new { id = TournamentId });
+        }
+    }
+}
--- Pages/Tournaments/Details.cshtml.cs
+++ Pages/Tournaments/Details.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,15 +14,18 @@ namespace RealTournament.Pages.Tournaments
     {
         private readonly RealTournamentContext _context;
         private readonly IdentityContext _identity;
+        private readonly UserManager<RealTournamentUser> _userManager;
 
-        public DetailsModel(RealTournamentContext context, IdentityContext identity)
+        public DetailsModel(RealTournamentContext context, IdentityContext identity, UserManager<RealTournamentUser> userManager)
         {
             _context = context;
             _identity = identity;
+            _userManager = userManager;
         }
 
         public Tournament Tournament { get; set; }
         public string Organizer { get; set; }
+        public bool CanApply { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -45,6 +49,10 @@ namespace RealTournament.Pages.Tournaments
             }
 
             Organizer = organizer.Name;
+
+            CanApply = !(await _context.Participant
+                .Where(p => p.TournamentId == id && p.UserId == _userManager.GetUserId(User))
+                .AnyAsync());
 
             return Page();
         }
